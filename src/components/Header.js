58d50edcec61956e8533d4bc817b9ed4c112a82b
Michael <michael27mic@gmail.com>
2024-01-16 14:20:16 +0000

    meta tags and Header set up

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,3 +1,21 @@
-const Header = () => {};
+import { Link } from "react-router-dom";
+
+const Header = () => {
+    return (
+        <header>
+            <img src="./images/logo.png" alt="logo" />
+            <nav>
+                <ul>
+                    <li><Link to="/">Home</Link></li>
+                    <li><Link to="/About">About</Link></li>
+                    <li><Link to="/Menu">Menu</Link></li>
+                    <li><Link to="/Reservations">Reservations</Link></li>
+                    <li><Link to="/OrderOnline">Order Online</Link></li>
+                    <li><Link to="/Login">Login</Link></li>
+                </ul>
+            </nav>
+        </header>
+    );
+};
 
 export default Header;
